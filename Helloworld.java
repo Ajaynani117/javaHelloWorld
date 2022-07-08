91be2f1b77a0c6d913b3e61349c4f4fb049d9b77
Ajaynani117 <sriramajay1173@gmail.com>
2022-07-08 10:10:16 +0100

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+public static void Helloworld {
+       public static void main (String[] args) {
+      // print "Hello,world"to the terminal window.
+      system.out.println("Hello, world ");
+      }
+     }
